c6164f26171038855db21fc975276945a7420611
Ayngaran Krishnamurthy <ayngaranrk@gmail.com>
2025-01-15 11:22:48 -0500

    String Split

---
+++ Important Python Codes/Data Structures/String/string.py
@@ -0,0 +1,13 @@
+val = "Hello World"
+val1 = "LeetCode"
+val2 = "Czechoslovakia"
+
+#To find the index of last occurence of a string:
+res = val2.rindex("a")
+#print(res)
+
+#To split string using a specified seperator
+res = val.rsplit(" ")
+    #OR
+res = val.split(" ")
+print (res)
